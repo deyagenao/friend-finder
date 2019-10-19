754cdc74403189ce505f88586106c1b152845643
Deya Genao <deyagenao@gmail.com>
2019-10-19 16:03:30 -0400

    created logic to determine the best possible match based on new survey results

--- app/data/friends.js
+++ app/data/friends.js
@@ -71,6 +71,51 @@ var friendsArray = [
     }
 ]
 
+// Create a function that receives the friendsArray as an argument and determines the best friend match
+
+
+// Set up a variable for the best match, should be an empty object 
+var bestFriend = {};
+
+function findBestFriend(obj, arr){
+
+    // set the best match to first equal the first person in the friendsArray 
+    bestFriend = arr[0];
+
+    // Set up a variable for the lowestTotalDifference equal to the highest possible difference from survey results 
+    var lowestTotalDifference = 40;
+
+    // Begin a for loop to compare the new results against all of the old results 
+    for (var i=0; i < arr.length; i++){
+
+        // set up the totalDifference variable to add the differences for each
+        var totalDifference = 0;
+
+        // For each comparison, run a for loop to compare all of the scores. 
+        for (var j=0; j < arr[i].scores.length; j++){
+
+            // For each index position, subtract the difference and convert to an absolute value. Add this value to the totalDifference variable 
+            totalDifference += Math.abs(parseInt(obj.scores[j]) - parseInt(arr[i].scores[j]));
+
+        }
+
+        // after all scores have been compared, if the totalDifference is less than the previous, set the best match variable equal to the current friend being compared 
+        if (totalDifference < lowestTotalDifference){
+            bestFriend = arr[i];
+            lowestTotalDifference = totalDifference
+        }
+    }
+    console.log(bestFriend);
+}
+
+var newFriend = {
+    name: "dg",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+};
+
+findBestFriend(newFriend, friendsArray);
+
+
 
 // Exporting the friendsArray data for use in api routes file
 module.exports = friendsArray;
